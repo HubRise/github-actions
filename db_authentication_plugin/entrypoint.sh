3800e6064ddadf4ae9a87a51680e15971e13b51e
Antoine Monnier <monniera@gmail.com>
2024-03-30 18:53:28 +0100

    Host: db -> localhost

--- db_authentication_plugin/entrypoint.sh
+++ db_authentication_plugin/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-mysql -h db -u root -pmysql -e "ALTER USER CURRENT_USER IDENTIFIED WITH mysql_native_password BY 'mysql'"
+mysql --host=localhost --port=3306 -u root -pmysql -e "ALTER USER CURRENT_USER IDENTIFIED WITH mysql_native_password BY 'mysql'"
